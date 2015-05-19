9a93e52a567500f25cf84db6045d3b2e887af272
Filshie <craigfilsh@gmail.com>
2015-05-19 18:20:34 +0100

    updated comments & typos

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,12 @@
 ## Programming Assignment 2
 
 ## These functions will find the inverse of a specified matrix (using Solve). 
-## If the inverse has already been calculated then display a message and return
-## the cached result instead. Saves on precious computational time!
+## If the inverse has already been calculated then will display a message and 
+## return the cached result instead to save on precious computational time
 
 
-## Funtion to create a special matrix object to cache its inverse.
+## Create a list containing functions to set/get the matrix value 
+## and set/get the inverse value
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -14,31 +15,30 @@ makeCacheMatrix <- function(x = matrix()) {
                 m <<- NULL
         }
         get <- function() x
-        setinverse <- function(inverse) m <<- inverse
-        getinverse <- function() m
+        set_inverse <- function(inverse) m <<- inverse
+        get_inverse <- function() m
         list(set = set, get = get, 
-             setinverse = setinverse, 
-             getinverse = getinverse)
+             set_inverse = set_inverse, 
+             get_inverse = get_inverse)
 }
 
 
 ## Takes returned matrix from makecCacheMatrix and checks if we've already 
-## calculated the inverse, if we have then return the cached result instead.
+## calculated the inverse, if we have then return the cached result instead
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        m <- x$getinverse()
+        m <- x$get_inverse()
         if(!is.null(m)) {
                 message("found cached data, grabbing!")
                 return(m)
         }
         data <- x$get()
         m <- solve(data, ...)
-        x$setinverse(m)
+        x$set_inverse(m)
         m
 }
 
-## Testing out the functions using a simple matrix
+## Results from testing out the functions using a simple matrix
 
 ## > my_matrix <- matrix(c(-1, 0, 2, 4, 6, 8, 10, 12, 14), 3, 3)
 ## > my_matrix
